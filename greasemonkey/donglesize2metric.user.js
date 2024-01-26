98cb7209c65b02c23ada36aabbf227b2d1b15fef
Yossi99 <yossi99.dev@gmail.com>
2024-01-26 18:57:37 -0300

    update bug fix version

--- greasemonkey/donglesize2metric.user.js
+++ greasemonkey/donglesize2metric.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DongleSize2Metric
 // @namespace    https://github.com/yossi99/DongleSize2Metric
-// @version      v0.0.2-beta
+// @version      v0.0.3-beta
 // @description  Convert the toy size chart information from imperial to metric system from some famous adult toy makers
 // @author       Yossi99
 // @match        https://twintailcreations.com/products/*
